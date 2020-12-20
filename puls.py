33e5c12ed01b688bae3a7f0b1db42ffcb0d099b1
alexaavr <45763511+alexaavr@users.noreply.github.com>
2020-12-20 14:31:57 +0200

    Add files via upload

--- puls.py
+++ puls.py
@@ -36,8 +36,8 @@ def read_temp_c():
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
-        temp_c = int(temp_string) / 1000.0 # TEMP_STRING IS THE SENSOR OUTPUT, MAKE SURE IT'S AN INTEGER TO DO THE MATH
-        temp_c = str(round(temp_c, 1)) # ROUND THE RESULT TO 1 PLACE AFTER THE DECIMAL, THEN CONVERT IT TO A STRING
+        temp_c = int(temp_string) / 1000.0
+        temp_c = str(round(temp_c, 1))
         return temp_c
 
 def read_max30100_hr():
@@ -50,18 +50,20 @@ def read_max30100_hr():
 
 def read_max30100_spo():
     mx30.read_sensor()
+    #mx30.set_spo_config(100,800)
     mx30.ir, mx30.red
+    print(mx30.ir)
     spo2 = 0
     Z = 0
     if mx30.ir != 0 and mx30.red != 0 :
-        Z = int((mx30.red/5)/(mx30.ir/5))
-        spo2 = 115 - 25*Z 
+        Z = float((mx30.red/5)/(mx30.ir/5))
+        spo2 = 133 - 25*Z 
     else: 
         if mx30.ir == 0 and mx30.red == 0:
             spo2 = 0
     print(spo2)
     if mx30.red != mx30.buffer_red:
-        return str(spo2)
+        return str(round(spo2,1))
 
 while True:
     if(int(read_max30100_hr()) == 0):
@@ -69,15 +71,15 @@ while True:
         display.lcd_display_string("Please place ", 1)
         display.lcd_display_string("your finger", 2)
     else:
-        if(int(read_max30100_spo())< 75):
+        if(float(read_max30100_spo())< 75):
             display.lcd_clear()
             display.lcd_display_string("Go to", 1)
             display.lcd_display_string("the doctor", 2)
         else:
-            if(int(read_max30100_spo())> 100):
+            if(float(read_max30100_spo())> 100):
                 display.lcd_clear
                 display.lcd_display_string("System", 1)
                 display.lcd_display_string("Error", 2)
             else:
                 display.lcd_display_string("Temp: " + read_temp_c() + "C", 1)
-                display.lcd_display_string("HR/SPO2: " + read_max30100_hr() + "/" + read_max30100_spo() + "%  ", 2)
+                display.lcd_display_string("HR/SPO2:" + read_max30100_hr() + "/" + read_max30100_spo() + "% ", 2)
